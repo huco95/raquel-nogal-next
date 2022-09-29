89b86c3e6e3dcc547eaa6f21869ff18a086090f2
Félix <hucoxileflagon@gmail.com>
2022-09-29 22:40:59 +0200

    refactor: added custom link style

--- components/Curriculum.js
+++ components/Curriculum.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 export default function Curriculum() {
   return (
     <div className="text-center">
-      <h2 className="mb-2 text-lg font-semibold text-gray-600">Currículum</h2>
+      <h2 className="mb-2 text-lg font-semibold text-gray-500 dark:text-gray-300">
+        Currículum
+      </h2>
 
       <div className="flex justify-center gap-x-10">
         <Link href="/curriculum/CV_RaquelNogal_ES.pdf">
           <a
-            className="flex items-center justify-center italic hover:underline gap-x-2"
+            className="flex items-center justify-center italic links gap-x-2"
             target="_blank"
           >
             Español
@@ -17,7 +19,7 @@ export default function Curriculum() {
 
         <Link href="/curriculum/CV_RaquelNogal_EN.pdf">
           <a
-            className="flex items-center justify-center italic hover:underline gap-x-2"
+            className="flex items-center justify-center italic links gap-x-2"
             target="_blank"
           >
             Inglés
