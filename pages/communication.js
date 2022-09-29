7a79e46ef7eae03015dfa1cb2c082d2080ad9c46
Félix <hucoxileflagon@gmail.com>
2022-09-29 17:11:34 +0200

    [WIP] feat: added missing pages

---
+++ pages/communication.js
@@ -0,0 +1,5 @@
+import Layout from "../components/Layout";
+
+export default function Communication() {
+  return <Layout></Layout>;
+}
---
+++ pages/marketing.js
@@ -0,0 +1,5 @@
+import Layout from "../components/Layout";
+
+export default function Marketing() {
+  return <Layout></Layout>;
+}
