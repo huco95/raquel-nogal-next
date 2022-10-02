85558a2138864d5c824ff18dcc8d9182e6706149
Félix <hucoxileflagon@gmail.com>
2022-10-02 19:02:47 +0200

    Added communication articles

--- pages/communication.js
+++ pages/communication.js
@@ -1,5 +1,7 @@
-import Head from "next/head";
+import ArticleCard from "../components/ArticleCard";
 import Layout from "../components/Layout";
+import Head from "next/head";
+import articles from "../content/communication/articles.json";
 
 export default function Communication() {
   return (
@@ -8,7 +10,22 @@ export default function Communication() {
         <title>Raquel Nogal - Comunicación</title>
       </Head>
 
-      <Layout></Layout>
+      <Layout>
+        <div className="grid grid-cols-2 gap-x-10 gap-y-16">
+          {articles.map((article, index) => {
+            return (
+              <ArticleCard
+                key={index}
+                title={article.title}
+                description={article.description}
+                publisher={article.publisher}
+                publishDate={article.date}
+                link={article.link}
+              />
+            );
+          })}
+        </div>
+      </Layout>
     </>
   );
 }
