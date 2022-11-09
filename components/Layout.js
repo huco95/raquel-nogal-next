1c7d5149c9451b4943a3c4ef8050bd7445127222
Félix <hucoxileflagon@gmail.com>
2022-11-09 21:05:01 +0100

    Keep x-padding on all screen sizes

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main className="max-w-5xl px-4 py-6 m-auto sm:px-0 min-h-main">
-        {children}
-      </main>
+      <main className="max-w-5xl px-4 py-6 m-auto min-h-main">{children}</main>
     </>
   );
 }
