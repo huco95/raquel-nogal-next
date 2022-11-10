326b1a2457bbc5464fd5c2cddb1205d578f56c0c
Félix <hucoxileflagon@gmail.com>
2022-11-10 17:53:41 +0100

    Updated footer divider

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Social from "./social/Social";
 export default function Footer() {
   return (
     <footer className="h-footer">
-      <div class="w-4/6 h-px bg-gray-200 m-auto mb-4"></div>
+      <div class="w-4/6 h-0.5 bg-gray-200 m-auto mb-4"></div>
       <div className="flex flex-row justify-center gap-x-4">
         <p className="font-medium tracking-wider">Raquel Nogal</p>
         <span>·</span>
