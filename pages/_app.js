a4a51e350b01a68bd87d6d5aee28af9fcd54de43
Félix <hucoxileflagon@gmail.com>
2023-01-28 19:56:09 +0100

    Add opengraph meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,56 @@
 import "../styles/globals.css";
-import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Raquel Nogal</title>
+        <title>Raquel Nogal: marketing y comunicación</title>
         <meta
           name="description"
           content="Raquel Nogal: marketing y comunicación"
         />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Raquel Nogal: marketing y comunicación</title>
+        <meta
+          name="description"
+          content="Raquel Nogal: marketing y comunicación"
+        />
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://raquel-nogal.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Raquel Nogal: marketing y comunicación"
+        />
+        <meta
+          property="og:description"
+          content="Raquel Nogal: marketing y comunicación"
+        />
+        <meta
+          property="og:image"
+          content="https://raquel-nogal.vercel.app/opengraph.png"
+        />
+        /{/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="raquel-nogal.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://raquel-nogal.vercel.app"
+        />
+        <meta
+          name="twitter:title"
+          content="Raquel Nogal: marketing y comunicación"
+        />
+        <meta
+          name="twitter:description"
+          content="Raquel Nogal: marketing y comunicación"
+        />
+        <meta
+          name="twitter:image"
+          content="https://raquel-nogal.vercel.app/opengraph.png"
+        />
       </Head>
       <Component {...pageProps} />
       <Analytics />
--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,7 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap"
           rel="stylesheet"
